c8634da7f0ce3ce40aed8190b6e09ccdf1239923
anushka255 <aachary2@ramapo.edu>
2023-09-28 11:09:29 -0400

    Updated tests to handle exceptions

--- tests/test_api_capital.py
+++ tests/test_api_capital.py
@@ -1,5 +1,8 @@
+import pytest
 from walrus.api import get_capital
 
 
 def test_capital():
     assert get_capital("Australia") == "Canberra"
+    assert get_capital("Austraalia") is None
+    with pytest.raises(ValueError, match=(f"No data found for country: Austraalia")):get_capital("Austraalia", True)
