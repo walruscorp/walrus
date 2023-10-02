b15987c169a7008c2d99d6ae26ddfa1acbf53e4d
anushka255 <aachary2@ramapo.edu>
2023-10-02 16:47:03 -0400

    Removed some tests functions that were using the pandas package

--- tests/test_api_capital.py
+++ tests/test_api_capital.py
@@ -1,46 +1,35 @@
 import pytest
-from walrus.api import get_capital, get_capital_2
+from walrus.api import get_capital
 
 
-def test_capital_correct():
+def test_capital_single_correct():
     assert get_capital("Australia") == "Canberra"
     assert get_capital("Australia", raise_errors=True) == "Canberra"
 
 
-def test_capital_incorrect():
-    assert get_capital("Austraalia") is None
-    with pytest.raises(ValueError, match=("No data found for country: Austraalia")):
-        get_capital("Austraalia", raise_errors=True)
-
-
-def test_capital_single_correct():
-    assert get_capital_2("Australia") == "Canberra"
-    assert get_capital_2("Australia", raise_errors=True) == "Canberra"
-
-
 def test_capital_tuple_correct():
-    assert get_capital_2(("Australia", "France")) == {"Australia": "Canberra", "France": "Paris"}
-    assert get_capital_2(("Australia", "France"), raise_errors=True) == {"Australia": "Canberra", "France": "Paris"}
+    assert get_capital(("Australia", "France")) == {"Australia": "Canberra", "France": "Paris"}
+    assert get_capital(("Australia", "France"), raise_errors=True) == {"Australia": "Canberra", "France": "Paris"}
 
 
 def test_capital_list_correct():
-    assert get_capital_2(["Australia", "France"]) == {"Australia": "Canberra", "France": "Paris"}
-    assert get_capital_2(["Australia", "France"], raise_errors=True) == {"Australia": "Canberra", "France": "Paris"}
+    assert get_capital(["Australia", "France"]) == {"Australia": "Canberra", "France": "Paris"}
+    assert get_capital(["Australia", "France"], raise_errors=True) == {"Australia": "Canberra", "France": "Paris"}
 
 
 def test_capital_single_incorrect():
-    assert get_capital_2("Austraalia") is None
-    with pytest.raises(ValueError, match=("No data found for country: Austraalia")):
+    assert get_capital("Austraalia") is None
+    with pytest.raises(ValueError, match="No data found for country: Austraalia"):
         get_capital("Austraalia", raise_errors=True)
 
 
 def test_capital_tuple_incorrect():
-    assert get_capital_2(("Australia", "Fraance")) == {"Australia": "Canberra", "Fraance": None}
-    with pytest.raises(ValueError, match=("No data found for country: Austraalia")):
-        get_capital_2(("Austraalia", "France"), raise_errors=True)
+    assert get_capital(("Australia", "Fraance")) == {"Australia": "Canberra", "Fraance": None}
+    with pytest.raises(ValueError, match="No data found for country: Austraalia"):
+        get_capital(("Austraalia", "France"), raise_errors=True)
 
 
 def test_capital_list_incorrect():
-    assert get_capital_2(["Australia", "Fraance"]) == {"Australia": "Canberra", "Fraance": None}
-    with pytest.raises(ValueError, match=("No data found for country: Austraalia")):
-        get_capital_2(["Austraalia", "France"], raise_errors=True)
+    assert get_capital(["Australia", "Fraance"]) == {"Australia": "Canberra", "Fraance": None}
+    with pytest.raises(ValueError, match="No data found for country: Austraalia"):
+        get_capital(["Austraalia", "France"], raise_errors=True)
