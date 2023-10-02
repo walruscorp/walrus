3b81f411334af4b935bcae16de45ca77926d78aa
anushka255 <aachary2@ramapo.edu>
2023-10-02 13:41:55 -0400

    Modified the get_capital function to take either a single country or multiple country names

--- walrus/api.py
+++ walrus/api.py
@@ -26,3 +26,50 @@ def get_capital(country_name, raise_errors=False):
     # Extract and return the capital from the matched row
     return country_data.iloc[0]['CapitalName']
 
+
+def get_capital_2(country_names, raise_errors=False):
+    # Dictionary to store country and capital as key-value pair
+    country_capital = {}
+
+    # Open the csv file with the reading mode
+    with open(file_path, 'r') as file:
+        # Split the file line by line and store it in a list
+        data = file.readlines()
+
+        # Store country and its capital as a key-value pair
+        for line in data:
+            values = line.split(",")
+            country_capital[values[0]] = values[1]
+
+    # Handle a single country name
+    if isinstance(country_names, str):
+        capital = country_capital.get(country_names)
+
+        # Check if any data was found
+        if capital is None:
+            if raise_errors:
+                raise ValueError(f"No data found for country: {country_names}")
+            else:
+                return None
+
+        # Extract and return the capital
+        return capital
+
+    # Handle multiple country names
+    else:
+        # Dictionary to store hte country and capitals of requested countries
+        results = {}
+
+        for country in country_names:
+            capital = country_capital.get(country)
+            if capital is None:
+                if raise_errors:
+                    raise ValueError(f"No data found for country: {country}")
+                else:
+                    results[country] = None
+            else:
+                results[country] = capital
+    return results
+
+
+
