1df31c1d66ef7dadb2d88ff05a85efc0be92a9e3
anushka255 <aachary2@ramapo.edu>
2023-10-03 10:28:29 -0400

    Refactored code to invoke file only once

--- walrus/api.py
+++ walrus/api.py
@@ -1,7 +1,9 @@
 import os
+from walrus.FileConfig import FileConfig
 
 file_dir = os.path.dirname(os.path.realpath(__file__))
 file_path = os.path.join(file_dir, 'data', 'country-capitals.csv')
+file_object = FileConfig(file_path)
 
 
 def hello():
@@ -9,37 +11,14 @@ def hello():
 
 
 def find_capital(country_name, raise_errors=False):
-    # Dictionary to store country and capital as key-value pair
-    country_capital = {}
+    capital = file_object.get_capital(country_name)
 
-    # Open the csv file with the reading mode
-    with open(file_path, 'r') as file:
-        # Split the file line by line and store it in a list
-        data = file.readlines()
-
-        # Header of the csv file
-        header = data[0].split(",")
-
-        # Indexes for country and capitals, respectively
-        country_col = header.index('CountryName')
-        capital_col = header.index('CapitalName')
-
-        # Ignore the first line
-        # Store country and its capital as a key-value pair
-        for line in data[1:]:
-            values = line.split(",")
-            country_capital[values[country_col]] = values[capital_col]
-
-    capital = country_capital.get(country_name)
-
-    # Check if any data was found
     if capital is None:
         if raise_errors:
             raise ValueError(f"No data found for country: {country_name}")
         else:
             return None
 
-    # Extract and return the capital
     return capital
 
 
@@ -50,7 +29,6 @@ def get_capital(country_names, raise_errors=False):
 
     # Handle multiple country names
     else:
-        # Dictionary to store the country and capitals of requested countries
         results = {}
 
         for country in country_names:
